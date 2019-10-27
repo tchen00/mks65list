8006a757dd74d08a437a6c8232d0a163f7518d8c
Tammy Chen <tchen00@stuy.edu>
2019-10-26 21:39:03 -0400

    finally done brain dead

--- list.c
+++ list.c
@@ -4,11 +4,12 @@
 
 void print_list(struct node * n){
   struct node *current = n;
+  printf("[ ");
   while (current != NULL){
-    printf("%d -> ", current -> i);
+    printf("%d ", current -> i);
     current = current -> next;
   }
-  printf("[ ]\n");
+  printf("]\n");
 }
 
 struct node * insert_front(struct node * n, int k){
@@ -16,4 +17,36 @@ struct node * insert_front(struct node * n, int k){
   new -> i = k;
   new -> next = n;
   return new;
+}
+
+struct node * free_list(struct node * n){
+  struct node *current = n;
+  struct node *holder = n;
+  while (current){
+    current = current -> next;
+    free(holder);
+    holder = current;
+  }
+  return current;
+}
+
+struct node * remove_node(struct node * front, int i){
+  struct node *temp = front;
+  struct node *prev = temp;
+  if(temp != NULL && temp->i == i) {
+    temp = temp->next;
+    free(temp);
+    front = temp->next;
+    return front;
+  }
+  while(temp != NULL && temp->i != i) {
+    prev = temp;
+    temp = temp->next;
+  }
+  if(temp == NULL) {
+    return front;
+  }
+  prev->next = temp->next;
+  free(temp);
+  return front;
 }
--- main.c
+++ main.c
@@ -7,13 +7,39 @@ int main(){
   printf("\n-------------------\n");
   printf("Printing empty list: \n");
   print_list(n);
-  printf("\n-------------------\n");
-
+  printf("-------------------\n");
+  printf("Printing list:\n");
   int i;
   for ( i = 0; i <= 9; i++){
     n = insert_front(n, i);
-    print_list(n);
+    // print_list(n);
   }
+  print_list(n);
+  printf("-------------------\n");
+  printf("Removing 8:\n");
+  n = remove_node(n,8);
+  print_list(n);
+  printf("-------------------\n");
+  printf("Removing 9:\n");
+  n = remove_node(n,9);
+  print_list(n);
+  printf("-------------------\n");
+  printf("Removing 4:\n");
+  n = remove_node(n,4);
+  print_list(n);
+  printf("-------------------\n");
+  printf("Removing 0:\n");
+  n = remove_node(n,0);
+  print_list(n);
+  printf("-------------------\n");
+  printf("Removing -1:\n");
+  n = remove_node(n,-1);
+  print_list(n);
+  printf("-------------------\n");
+  printf("Freeing List:\n");
+  n = free_list(n);
+  printf("Printing empty list:\n");
+  print_list(n);
 
   return 0;
 }
